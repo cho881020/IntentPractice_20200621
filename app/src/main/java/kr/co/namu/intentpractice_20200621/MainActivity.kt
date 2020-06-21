7e080dbc67da165eb5d3087088c9a577118c328b
조경진 <kj_cho@idealidea.co.kr>
2020-06-21 19:57:16 +0900

    숫자 대신 변수를 이용해서 어떤 요청인지 쉽게 이해할 수 있도록 코드 변경

--- app/src/main/java/kr/co/namu/intentpractice_20200621/MainActivity.kt
+++ app/src/main/java/kr/co/namu/intentpractice_20200621/MainActivity.kt
@@ -7,6 +7,9 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQ_FOR_NICKNAME = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -49,7 +52,7 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this, EditNickNameActivity::class.java)
 //            결과를 가질러 가는 이동은 startActivityForResult로 가야함
 //            이런방식의 이동은 어떤결과를 가지러 (닉네임 / 비번 등) 가는지 숫자로 구별해야함. Ex. 닉네임-1000
-            startActivityForResult(myIntent, 1000)
+            startActivityForResult(myIntent, REQ_FOR_NICKNAME)
         }
 
     }
@@ -60,7 +63,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
 //        이 함수가 실행된 이유 (어떤 요청?) => 1000번이었는지? => 닉네임 변경을 위해 다녀온건지?
-        if (requestCode == 1000) {
+        if (requestCode == REQ_FOR_NICKNAME) {
 
 //            확인을 누른게 맞는지? => 취소를 한게 아닌지?
             if (resultCode == Activity.RESULT_OK) {
